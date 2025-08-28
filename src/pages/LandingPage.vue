ab6d022880f8ffaa104f15ee1dfd9c82777d0697
agampreetsingh01 <agampreetsingh01@gmail.com>
2025-08-28 13:01:45 +0000

    landing layout

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -356,7 +356,7 @@ const scrollToTop = () => {
     right: 5%;
     height: 80%;
     width: 40%;
-    background: url(../assets/peshkash-qr.png) center / contain no-repeat;
+    background: url(../assets/peshkashqrhero.png) center / contain no-repeat;
     opacity: 0.4;
   }
 
