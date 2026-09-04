3b89ba8770342e2df978d51c01daf776e547ad52
Agampreet Singh <agampreet_singh@yahoo.com>
2026-09-05 00:43:33 +0530

    Use official brand-kit logo in social previews

--- scripts/build-social-preview-assets.ps1
+++ scripts/build-social-preview-assets.ps1
@@ -20,11 +20,24 @@ function New-Canvas([string]$background) {
   $bitmap = [System.Drawing.Bitmap]::new(1200, 630)
   $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
   $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
+  $graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+  $graphics.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
   $graphics.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAliasGridFit
   $brush = New-Brush $background
   $graphics.FillRectangle($brush, 0, 0, 1200, 630)
   $brush.Dispose()
   return @($bitmap, $graphics)
+}
+
+function Draw-PeshkashLogo([System.Drawing.Graphics]$graphics, [int]$x, [int]$y, [int]$width) {
+  $logoPath = Join-Path $PSScriptRoot 'assets\Peshkash-Primary-For-Dark-2000px.png'
+  $logo = [System.Drawing.Bitmap]::FromFile([System.IO.Path]::GetFullPath($logoPath))
+  # Exact visible bounds of the approved Primary-For-Dark brand-kit lockup.
+  $source = [System.Drawing.Rectangle]::new(332, 155, 1377, 437)
+  $height = [Math]::Round($width * $source.Height / $source.Width)
+  $destination = [System.Drawing.Rectangle]::new($x, $y, $width, $height)
+  $graphics.DrawImage($logo, $destination, $source, [System.Drawing.GraphicsUnit]::Pixel)
+  $logo.Dispose()
 }
 
 $serif = 'Georgia'
@@ -40,7 +53,7 @@ $muted = New-Brush '#C8B6A3'
 $graphics.FillRectangle($gold, 60, 60, 8, 510)
 $graphics.DrawEllipse([System.Drawing.Pen]::new([System.Drawing.ColorTranslator]::FromHtml('#5C4936'), 2), 895, 72, 235, 235)
 $graphics.DrawEllipse([System.Drawing.Pen]::new([System.Drawing.ColorTranslator]::FromHtml('#8A6A48'), 2), 945, 122, 235, 235)
-$graphics.DrawString('PESHKASH', [System.Drawing.Font]::new($sans, 22, [System.Drawing.FontStyle]::Bold), $gold, 108, 82)
+Draw-PeshkashLogo $graphics 100 70 240
 $graphics.DrawString('Your shop window,', [System.Drawing.Font]::new($serif, 70, [System.Drawing.FontStyle]::Regular), $paper, 105, 206)
 $graphics.DrawString('digitally', [System.Drawing.Font]::new($serif, 90, [System.Drawing.FontStyle]::Italic), $paper, 105, 292)
 $graphics.DrawString('Open. Save. Share.', [System.Drawing.Font]::new($sans, 25, [System.Drawing.FontStyle]::Regular), $muted, 111, 464)
@@ -62,7 +75,7 @@ for ($i = 0; $i -lt 4; $i++) {
   $graphics.DrawRectangle([System.Drawing.Pen]::new([System.Drawing.ColorTranslator]::FromHtml('#BFA98E'), 2), $x, 82, 110, 378)
 }
 $paper = New-Brush '#F8F2E9'
-$graphics.DrawString('PESHKASH', [System.Drawing.Font]::new($sans, 20, [System.Drawing.FontStyle]::Bold), $gold, 58, 65)
+Draw-PeshkashLogo $graphics 58 62 220
 $graphics.DrawString('for', [System.Drawing.Font]::new($serif, 42, [System.Drawing.FontStyle]::Italic), $paper, 58, 164)
 $graphics.DrawString('exhibitions', [System.Drawing.Font]::new($serif, 48, [System.Drawing.FontStyle]::Regular), $paper, 58, 218)
 $graphics.DrawString('Every stall.', [System.Drawing.Font]::new($serif, 29, [System.Drawing.FontStyle]::Regular), $paper, 61, 372)
